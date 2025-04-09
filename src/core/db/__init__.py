f9b6fbb39fb0825d4608f1dc0aaf9e08e5e2ef7b
altair <altairgeo000@gmail.com>
2025-04-09 13:41:11 +0000

    set base model, config from .env, alchemyEngine, and sessions

---
+++ src/core/db/__init__.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from src.config import settings
+from .models import BaseModel
+
+# -------------------------------------------------------------------------------
+#  CREATION ENGINE AND SESSION
+
+try:
+    engine = create_async_engine(
+        settings.db_url,
+        pool_size=20,
+        max_overflow=0,
+        pool_recycle=300,
+        pool_pre_ping=True
+    )
+except Exception as e:
+    print(e)
+    try:
+        engine = create_async_engine(settings.db_url)
+    except Exception as e:
+        print(e)
+
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)  # Create session maker
+async def get_async_session():
+    async with async_session_maker() as session:
+        yield session
+
+# ----------------------------------------------------------------------------------
