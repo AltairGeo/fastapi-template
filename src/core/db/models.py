093ef4d3e9c91adc7297c9f42c9a5392a4c10676
AltairGeo <altairgeo000@gmail.com>
2025-04-11 03:34:53 +0300

    add pychache to gitignore, create user model and migration

--- src/core/db/models.py
+++ src/core/db/models.py
@@ -1,6 +1,12 @@
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
+from fastapi_users.db import SQLAlchemyBaseUserTable
+from sqlalchemy import Column, Integer
 
 
 class BaseModel(AsyncAttrs, DeclarativeBase):
     pass
+
+
+class User(SQLAlchemyBaseUserTable[int], BaseModel):
+    id = Column(Integer, primary_key=True)
