c3e59e479f9a0c019da97a2994502d0fc5c48236
AltairGeo <altairgeo000@gmail.com>
2025-04-12 02:01:06 +0300

    UserManager and schemas

--- src/config.py
+++ src/config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     db_url: str = "sqlite+aiosqlite:///database.db"
     token_lifetime: int = 3600 
+    app_secret_reset_password: str
+    app_secret_verify: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
--- src/core/auth.py
+++ src/core/auth.py
@@ -1,5 +1,11 @@
 from fastapi_users.authentication import BearerTransport, AuthenticationBackend
-from .db import get_database_stategy
+from .db import get_database_stategy, get_user_db
+from .db.models import User
+from fastapi_users import BaseUserManager, IntegerIDMixin
+from src.config import settings
+from fastapi import Depends, Request
+from typing import Optional
+
 
 
 bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
@@ -10,3 +16,25 @@ auth_backend = AuthenticationBackend(
     get_strategy=get_database_stategy,
 )
 
+
+class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
+    reset_password_token_secret = settings.app_secret_reset_password
+    verification_token_secret = settings.app_secret_verify
+
+    async def on_after_register(self, user: User, request: Optional[Request] = None):
+        print(f"User {user.id} has registered.")
+
+    async def on_after_forgot_password(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        print(f"User {user.id} has forgot their password. Reset token: {token}")
+
+    async def on_after_request_verify(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        print(f"Verification requested for user {user.id}. Verification token: {token}")
+
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
